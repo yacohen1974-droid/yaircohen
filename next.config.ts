ccabedd30c59d6e22722aa8d0de070c3a5448fa6
Amir Eyal <amirher@gmail.com>
2026-07-17 19:34:31 +0300

    Allow Next.js to render images from postimg.cc domain

--- next.config.ts
+++ next.config.ts
@@ -49,6 +49,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
